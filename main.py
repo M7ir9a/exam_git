c7e620a379ba7b46163e151111bd06acd2bb9621
Abdou <ferrag.abdennour@gmail.com>
2025-10-16 10:21:01 +0100

    test partie avancé

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from modules.utils import hello
 print(hello("Git user"))
-from modules.math_tools import multiply print("Résultat du calcul :", multiply(4, 5))
+from modules.math_tools import multiply
+print("Résultat du calcul :", multiply(4, 5))
